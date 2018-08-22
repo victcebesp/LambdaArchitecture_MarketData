c9701f643ff7e7a204cd3dddc9fcef92c104bb57
Víctor Ceballos <victcebesp@gmail.com>
2018-08-22 09:53:15 +0200

    Refactor: Indexes generation delegated to IndexesGenerator class

---
+++ feeder/src/org/eifer/market/generator/IndexesGenerator.java
@@ -0,0 +1,85 @@
+package org.eifer.market.generator;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class IndexesGenerator {
+
+    private final Map<String, BiConsumer<String, Map<String, Integer>>> fieldFillers;
+
+    public IndexesGenerator() {
+        fieldFillers = fillFieldFiller();
+    }
+
+    public Map<String, Integer> getIndexes(List<String> indexesRecords) {
+        return fillIndexesWith(indexesRecords);
+    }
+
+    private Map<String, BiConsumer<String, Map<String, Integer>>> fillFieldFiller() {
+        Map<String, BiConsumer<String, Map<String, Integer>>> fieldFillers;
+        fieldFillers = new HashMap<>();
+        fieldFillers.put("GCIL", this::fillGcilFields);
+        fieldFillers.put("GUIL", this::fillGuilFields);
+        fieldFillers.put("PCIL", this::fillPcilFields);
+        fieldFillers.put("COIL", this::fillCoilFields);
+        return fieldFillers;
+    }
+
+    private Map<String, Integer> fillIndexesWith(List<String> indexesRecords) {
+
+        Map<String, Integer> indexes = new HashMap<>();
+
+        indexesRecords.forEach(e -> {
+            String key = e.split(";")[0].substring(2).equals("ICIL") ? "GCIL" : e.split(";")[0].substring(2);
+            fieldFillers.get(key).accept(e, indexes);
+        });
+
+        return indexes;
+    }
+
+    private void fillGcilFields(String indexRecord, Map<String, Integer> indexes) {
+        String[] splittedRecord = indexRecord.split(";");
+        for (int i = 0; i < splittedRecord.length; i++){
+            if (splittedRecord[i].equals("[TimeStamp]")) indexes.put("ts", i);
+            else if (splittedRecord[i].equals("[Capacity]") || splittedRecord[i].equals("[InstalledCapacity]")) indexes.put("gcilCapacity", i);
+            else if (splittedRecord[i].equals("[UnitID]")) indexes.put("gcilUnitID", i);
+        }
+    }
+
+    private void fillGuilFields(String indexRecord, Map<String, Integer> indexes) {
+        String[] splittedRecord = indexRecord.split(";");
+        for (int i = 0; i < splittedRecord.length; i++){
+            if (splittedRecord[i].equals("[UnitID]")) indexes.put("guilUnitID", i);
+            else if (splittedRecord[i].equals("[ProdConsID]")) indexes.put("guilPlantID", i);
+            else if (splittedRecord[i].equals("[UnitName]")) indexes.put("guilUnitName", i);
+            else if (splittedRecord[i].equals("[ConnectingArea]")) indexes.put("guilConnectingArea", i);
+            else if (splittedRecord[i].equals("[Source]")) indexes.put("guilSource", i);
+            else if (splittedRecord[i].equals("[StartDate]")) indexes.put("guilStartDate", i);
+            else if (splittedRecord[i].equals("[EndDate]")) indexes.put("guilEndDate", i);
+        }
+    }
+
+    private void fillPcilFields(String indexRecord, Map<String, Integer> indexes) {
+        String[] splittedRecord = indexRecord.split(";");
+        for (int i = 0; i < splittedRecord.length; i++){
+            if (splittedRecord[i].equals("[CompanyID]")) indexes.put("pcilCompanyID", i);
+            else if (splittedRecord[i].equals("[ProdConsID]")) indexes.put("pcilPlantID", i);
+            else if (splittedRecord[i].equals("[ProdConsName]")) indexes.put("pcilPlantName", i);
+            else if (splittedRecord[i].equals("[WGS84Latitude]")) indexes.put("pcilLatitude", i);
+            else if (splittedRecord[i].equals("[WGS84Longitude]")) indexes.put("pcilLongitude", i);
+            else if (splittedRecord[i].equals("[Country]")) indexes.put("pcilCountry", i);
+            else if (splittedRecord[i].equals("[ReportingReason]")) indexes.put("pcilReportingReason", i);
+        }
+    }
+
+    private void fillCoilFields(String indexRecord, Map<String, Integer> indexes) {
+        String[] splittedRecord = indexRecord.split(";");
+        for (int i = 0; i < splittedRecord.length; i++){
+            if (splittedRecord[i].equals("[CompanyID]")) indexes.put("coilCompanyID", i);
+            else if (splittedRecord[i].equals("[CompanyName]")) indexes.put("coilCompanyName", i);
+        }
+    }
+
+}
--- feeder/src/org/eifer/market/generator/MasterDataUnitGenerator.java
+++ feeder/src/org/eifer/market/generator/MasterDataUnitGenerator.java
@@ -4,51 +4,21 @@ import org.eifer.box.schemas.MasterDataUnit;
 import org.eifer.market.parser.MasterDataDateTimeParser;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class MasterDataUnitGenerator {
 
-    private int ts;
-    private int guilUnitID;
-    private int capacity;
-    private int unitName;
-    private int guilPlantID;
-    private int connectingArea;
-    private int source;
-    private int startDate;
-    private int endDate;
-    private int latitude;
-    private int longitude;
-    private int country;
-    private int reportingReason;
-    private int coilCompanyName;
-    private int coilCompanyID;
-    private int gcilUnitID;
-    private int pcilCompanyID;
-    private int pcilPlantID;
-    private int pcilPlantName;
-    private final Map<String, Consumer<String>> fieldFillers;
+    private final Map<String, Integer> indexes;
 
     public MasterDataUnitGenerator(List<String> indexesRecords) {
-        fieldFillers = fillFieldFiller();
-        fillIndexesWith(indexesRecords);
-    }
-
-    private void fillIndexesWith(List<String> indexesRecords) {
-        indexesRecords.forEach(e -> {
-            String key = e.split(";")[0].substring(2).equals("ICIL") ? "GCIL" : e.split(";")[0].substring(2);
-            fieldFillers.get(key).accept(e);
-        });
+         indexes = new IndexesGenerator().getIndexes(indexesRecords);
     }
 
     public List<MasterDataUnit> generateAllEvents(Map<String, List<String>> classifiedRecords) {
 
         List<MasterDataUnit> masterDataUnits = new ArrayList<>();
-        int first = 0;
 
         List<String> generationCapacityRecords = classifiedRecords.get("GCIL");
         List<String> generationUnitsRecords = classifiedRecords.get("GUIL");
@@ -58,125 +28,58 @@ public class MasterDataUnitGenerator {
         generationCapacityRecords.forEach(e -> {
 
             String [] gcilFields = e.split(";");
-            MasterDataUnit masterDataUnit = new MasterDataUnit();
 
-            updateMasterDataUnitGcilFields(gcilFields, masterDataUnit);
+            Optional<String> guilRecord = generationUnitsRecords.stream()
+                    .filter(r -> r.split(";")[indexes.get("guilUnitID")].equals(gcilFields[indexes.get("gcilUnitID")]))
+                    .findFirst();
 
-            List<String> guilRecords = generationUnitsRecords.stream()
-                    .filter(r -> r.split(";")[guilUnitID].equals(gcilFields[gcilUnitID]))
-                    .collect(Collectors.toList());
+            if (!guilRecord.isPresent()) return;
 
-            if (guilRecords.isEmpty()) return;
+            String[] guilFields = guilRecord.get().split(";");
 
-            String guil = guilRecords.get(first);
+            Optional<String> pcilRecord = producerConsumerRecords.stream()
+                    .filter(r -> r.split(";")[indexes.get("pcilPlantID")].equals(guilFields[indexes.get("guilPlantID")]))
+                    .findFirst();
 
-            String[] splittedGuil = guil.split(";");
-            updateMasterDataUnitGuilFields(masterDataUnit, splittedGuil);
+            if (!pcilRecord.isPresent()) return;
 
+            String[] pcilFields = pcilRecord.get().split(";");
 
-            String pcil = producerConsumerRecords.stream()
-                    .filter(r -> r.split(";")[pcilPlantID].equals(splittedGuil[guilPlantID]))
-                    .collect(Collectors.toList()).get(first);
+            Optional<String> coilRecord = companyRecords.stream()
+                    .filter(r -> r.split(";")[indexes.get("coilCompanyID")].equals(pcilFields[indexes.get("pcilCompanyID")]))
+                    .findFirst();
 
-            String[] splittedPcil = pcil.split(";");
-            updateMasterDataUnitPcilFields(masterDataUnit, splittedPcil);
+            if (!coilRecord.isPresent()) return;
 
-            String coil = companyRecords.stream()
-                    .filter(r -> r.split(";")[coilCompanyID].equals(splittedPcil[pcilCompanyID]))
-                    .collect(Collectors.toList()).get(first);
+            String[] coilFields = coilRecord.get().split(";");
 
-            String[] splittedCoil = coil.split(";");
-
-            updateMasterDataUnitCoilFields(masterDataUnit, splittedCoil);
-
-            masterDataUnits.add(masterDataUnit);
+            masterDataUnits.add(createMasterDataUnit(gcilFields, guilFields, pcilFields, coilFields));
 
         });
 
         return masterDataUnits;
     }
 
-    private void updateMasterDataUnitGcilFields(String[] fields, MasterDataUnit masterDataUnit) {
-        masterDataUnit.unitID(fields[guilUnitID]);
-        masterDataUnit.capacity(Double.parseDouble(fields[capacity].replace(',', '.')));
-        masterDataUnit.ts(new MasterDataDateTimeParser().parseDateTimeToInstant(fields[ts]));
+    private MasterDataUnit createMasterDataUnit(String[] gcilFields, String[] guilFields, String[] pcilFields, String[] coilFields) {
+        return new MasterDataUnit()
+            .unitID(gcilFields[indexes.get("guilUnitID")])
+            .capacity(Double.parseDouble(gcilFields[indexes.get("gcilCapacity")].replace(',', '.')))
+            .ts(new MasterDataDateTimeParser().parseDateTimeToInstant(gcilFields[indexes.get("ts")]))
+            .source(guilFields[indexes.get("guilSource")])
+            .connectingArea(guilFields[indexes.get("guilConnectingArea")])
+            .startDate(guilFields[indexes.get("guilStartDate")])
+            .endDate(guilFields[indexes.get("guilEndDate")])
+            .unitName(guilFields[indexes.get("guilUnitName")])
+            .plantID(guilFields[indexes.get("guilPlantID")])
+            .country(pcilFields[indexes.get("pcilCountry")])
+            .latitude(pcilFields[indexes.get("pcilLatitude")])
+            .longitude(pcilFields[indexes.get("pcilLongitude")])
+            .companyID(pcilFields[indexes.get("coilCompanyID")])
+            .reportReason(pcilFields[indexes.get("pcilReportingReason")])
+            .plantName(pcilFields[indexes.get("pcilPlantName")])
+            .companyName(coilFields[indexes.get("coilCompanyName")]);
     }
 
-    private void updateMasterDataUnitGuilFields(MasterDataUnit masterDataUnit, String[] splittedGuil) {
-        masterDataUnit.source(splittedGuil[source]);
-        masterDataUnit.connectingArea(splittedGuil[connectingArea]);
-        masterDataUnit.startDate(splittedGuil[startDate]);
-        masterDataUnit.endDate(splittedGuil[endDate]);
-        masterDataUnit.unitName(splittedGuil[unitName]);
-        masterDataUnit.plantID(splittedGuil[guilPlantID]);
-    }
-
-    private void updateMasterDataUnitPcilFields(MasterDataUnit masterDataUnit, String[] splittedPcil) {
-        masterDataUnit.country(splittedPcil[country]);
-        masterDataUnit.latitude(splittedPcil[latitude]);
-        masterDataUnit.longitude(splittedPcil[longitude]);
-        masterDataUnit.companyID(splittedPcil[coilCompanyID]);
-        masterDataUnit.reportReason(splittedPcil[reportingReason]);
-        masterDataUnit.plantName(splittedPcil[pcilPlantName]);
-    }
-
-    private void updateMasterDataUnitCoilFields(MasterDataUnit masterDataUnit, String [] splittedCoil) {
-        masterDataUnit.companyName(splittedCoil[coilCompanyName]);
-    }
-
-    private void fillGcilFields(String indexRecord) {
-        String[] splittedRecord = indexRecord.split(";");
-        for (int i = 0; i < splittedRecord.length; i++){
-            if (splittedRecord[i].contains("TimeStamp")) ts = i;
-            if (splittedRecord[i].contains("Capacity")) capacity = i;
-            if (splittedRecord[i].contains("UnitID")) gcilUnitID = i;
-        }
-    }
-
-    private void fillGuilFields(String indexRecord) {
-        String[] splittedRecord = indexRecord.split(";");
-        for (int i = 0; i < splittedRecord.length; i++){
-            if (splittedRecord[i].contains("UnitID")) guilUnitID = i;
-            if (splittedRecord[i].contains("ProdConsID")) guilPlantID = i;
-            if (splittedRecord[i].contains("UnitName")) unitName = i;
-            if (splittedRecord[i].contains("ConnectingArea")) connectingArea = i;
-            if (splittedRecord[i].contains("Source")) source = i;
-            if (splittedRecord[i].contains("StartDate")) startDate = i;
-            if (splittedRecord[i].contains("EndDate")) endDate = i;
-        }
-    }
-
-    private void fillPcilFields(String indexRecord) {
-        String[] splittedRecord = indexRecord.split(";");
-        for (int i = 0; i < splittedRecord.length; i++){
-            if (splittedRecord[i].contains("CompanyID")) pcilCompanyID = i;
-            if (splittedRecord[i].contains("ProdConsID")) pcilPlantID = i;
-            if (splittedRecord[i].contains("ProdConsName")) pcilPlantName = i;
-            if (splittedRecord[i].contains("WGS84Latitude")) latitude = i;
-            if (splittedRecord[i].contains("WGS84Longitude")) longitude = i;
-            if (splittedRecord[i].contains("Country")) country = i;
-            if (splittedRecord[i].contains("ReportingReason")) reportingReason = i;
-        }
-    }
-
-    private void fillCoilFields(String indexRecord) {
-        String[] splittedRecord = indexRecord.split(";");
-        for (int i = 0; i < splittedRecord.length; i++){
-            if (splittedRecord[i].contains("CompanyID")) coilCompanyID = i;
-            if (splittedRecord[i].contains("CompanyName")) coilCompanyName = i;
-        }
-    }
-
-
-    private Map<String, Consumer<String>> fillFieldFiller() {
-        Map<String, Consumer<String>> fieldFillers;
-        fieldFillers = new HashMap<>();
-        fieldFillers.put("GCIL", this::fillGcilFields);
-        fieldFillers.put("GUIL", this::fillGuilFields);
-        fieldFillers.put("PCIL", this::fillPcilFields);
-        fieldFillers.put("COIL", this::fillCoilFields);
-        return fieldFillers;
-    }
 }
 
 
